ea7a4976f03d6e2c7fd9eb1c8cadfcbe483a56a4
Reddyong <d0209hong@naver.com>
2024-03-22 11:31:06 +0900

    feat : queue practice using linked list

---
+++ src/fc/datastructure/ch04/IQueue.java
@@ -0,0 +1,15 @@
+package fc.datastructure.ch04;
+
+public interface IQueue<T> {
+    void offer(T data);
+
+    T poll();
+
+    T peek();
+
+    int size();
+
+    void clear();
+
+    boolean isEmpty();
+}
---
+++ src/fc/datastructure/ch04/MyLinkedQueue.java
@@ -0,0 +1,79 @@
+package fc.datastructure.ch04;
+
+public class MyLinkedQueue<T> implements IQueue<T> {
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public MyLinkedQueue() {
+        this.size = 0;
+        this.head = new Node(null);
+        this.tail = this.head;
+    }
+
+    @Override
+    public void offer(T data) {
+        Node node = new Node(data);
+        this.tail.next = node;
+        this.tail = this.tail.next;
+        this.size++;
+    }
+
+    @Override
+    public T poll() {
+        if (this.isEmpty()) {
+            throw new IllegalStateException();
+        }
+
+        Node node = this.head.next;
+        this.head.next = node.next;
+        node.next = null;
+        this.size--;
+
+        if (this.head.next == null) {
+            this.tail = this.head;
+        }
+
+        return node.data;
+    }
+
+    @Override
+    public T peek() {
+        if (this.isEmpty()) {
+            throw new IllegalStateException();
+        }
+
+        return this.head.next.data;
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public void clear() {
+        this.size = 0;
+        this.head.next = null;
+        this.tail = this.head;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.head.next == null;
+    }
+
+    private class Node {
+        T data;
+        Node next;
+
+        public Node(T data) {
+            this.data = data;
+        }
+
+        public Node(T data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+}
